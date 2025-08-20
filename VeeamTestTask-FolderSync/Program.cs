a52cab5b80cf20a27ee31cad2bd479ce18b4595b
Malashchuk Vladyslav <vladok.malashuk@gmail.com>
2025-08-20 11:59:55 +0200

    Add project files.

---
+++ VeeamTestTask-FolderSync/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VeeamTestTask_FolderSync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
