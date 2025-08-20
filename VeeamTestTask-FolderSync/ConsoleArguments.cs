338c75e547ac04c83411fa1b557799313fed4e62
Malashchuk Vladyslav <vladok.malashuk@gmail.com>
2025-08-20 15:11:49 +0200

    refactor: naming

--- VeeamTestTask-FolderSync/ConsoleArguments.cs
+++ VeeamTestTask-FolderSync/ConsoleArguments.cs
@@ -9,7 +9,7 @@ namespace VeeamTestTask_FolderSync
 
         [Option('r', "replica", Required = true, HelpText = "Path to replica folder.")]
         public required string replicaFolder { get; set; }
-        [Option('i', "interval", Required = true, HelpText = "Synchronization interval in minutes.")]
+        [Option('i', "interval", Required = true, HelpText = "Synchronization interval in milliseconds.")]
         public required TimeSpan syncInterval { get; set; }
         [Option('l', "log", Required = true, HelpText = "Path to log file.")]
         public required string log { get; set; }
